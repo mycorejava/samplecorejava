749bb1b43361a001303160d864e6c7c46b8b1dcd
Core Java Programs for Practice <49262711+mycorejava@users.noreply.github.com>
2019-06-09 14:58:21 +0530

    Update ChromeDesiredCapability.java

--- student1/ChromeDesiredCapability.java
+++ student1/ChromeDesiredCapability.java
@@ -1,4 +1,15 @@
 
+    public String path_chromedriver = "browserDriver//chromedriver_automation";
+    public String path_gecodriver   = "browserDriver//geckodriver_automation";
+
+    public final String path_chromelinux  = "/opt/google/chrome/chrome";
+    public final String path_chromewindows= "";
+    public final String path_chromemac    = "";
+
+    public final String path_firefoxlinux = "";
+    public final String path_firefoxwindows= "";
+    public final String path_firefoxmac   = "";
+
 
     public WebDriver initChromeBrowser(boolean bLoadImages){
         String strOSType = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH);
